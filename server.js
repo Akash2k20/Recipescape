92fd2de9e0498a4e1cddfb6ad063b9cbf04ff20b
Akash2k20 <akashlearning2020@gmail.com>
2023-03-07 22:30:12 +0530

    Deployment

--- server.js
+++ server.js
@@ -4,10 +4,27 @@ const morgan = require("morgan");
 const cors = require("cors");
 const RecipeRoute = require("./routes/reciperoute")
 const UserRoute = require("./routes/userroute")
+const path = require("path");
+const app = express();
+
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+
+app.use(express.static(buildPath));
+
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 const db = require("./database/database");
 
-const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json())
