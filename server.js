c7690caeb17b517336760c61a8a147ee0b52875f
Akash2k20 <akashlearning2020@gmail.com>
2023-03-08 08:45:40 +0530

    Idk

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ const UserRoute = require("./routes/userroute")
 const path = require("path");
 const app = express();
 
-const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "../client/build");
+// const _dirname = path.dirname("");
+// const buildPath = path.join(_dirname, "../client/build");
 
-app.use(express.static(buildPath));
+// app.use(express.static(buildPath));
 
-app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile(
+//     path.join(__dirname, "../client/build/index.html"),
+//     function (err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     }
+//   );
+// });
 
 const db = require("./database/database");
 
